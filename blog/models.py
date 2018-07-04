8747533649a28a398d7de551ec5523f1d5699b2a
kuz <savage00817@gmail.com>
2018-07-04 15:54:06 +0900

    My Django Girls app, first commit

---
+++ blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True,null=True)
+    
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+        
+    def __str__(self):
+        return self.title
+
+'''
+class는 특별한 키워드로, 객체를 정의한다는 것을 알려줍니다.
+Post는 모델의 이름입니다. (특수문자와 공백 제외한다면) 다른 이름을 붙일 수도 있습니다. 항상 클래스 이름의 첫 글자는 대문자로 써야 합니다.
+models은 Post가 장고 모델임을 의미합니다. 이 코드 때문에 장고는 Post가 데이터베이스에 저장되어야 한다고 알게 됩니다.
+'''
